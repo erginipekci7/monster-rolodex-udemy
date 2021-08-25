7d4677f6f8fa7d682225ba18255669aaf960ab11
Ergin İpekçi <ergin.ipekci@arutesolutions.com>
2021-08-25 18:00:23 +0300

    Card-List component is created.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { CardList } from './components/card-list/card-list.component';
 import logo from './logo.svg';
 import './App.css';
 import { render } from '@testing-library/react';
@@ -9,31 +10,24 @@ class App extends Component {
   constructor(){
     super();
     this.state = {
-      monsters: [
-        {
-          name : 'Frankenstein',
-          id:'asc1'
-        },
-        {
-          name : 'Dracula',
-          id:'asc2'
-        },
-        {
-          name : 'Zombie',
-          id:'asc3'
-        },
-      ]
+      monsters: []
     }
-
+  }
+  componentDidMount(){
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(response=>response.json())  
+    .then(users=>this.setState({monsters : users}));
   }
 
 
   render(){
     return(
       <div className="App">
+        <CardList name="Ergin">
         {
           this.state.monsters.map(monster => <h1 key={monster.id}> {monster.name}</h1>)
         }
+        </CardList>
       </div>
     );
   }
---
+++ src/components/card-list/card-list.component.jsx
@@ -0,0 +1,5 @@
+import './card-list.style.css';
+export const CardList = (props) => {
+    console.log(props);
+    return <div className='card-list'>{props.children}</div>;
+} 
